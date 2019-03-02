100f204f123676ba9e5e4338eea10f3eda38b9c9
Uzair Inamdar <uzair_inamdar@hotmail.com>
2019-03-01 20:24:34 -0800

    Complete chord implementation

--- src/worker.py
+++ src/worker.py
@@ -2,7 +2,7 @@ from threading import Thread
 from address import Address
 import socket
 import sys
-M = 20
+from state import M
 
 def in_range(a, b, c, start=False, end=False):
   bla = None
@@ -38,10 +38,14 @@ class Worker(Thread):
     request = data.decode('utf-8').split(' ')
     print(request)
     if request[0] == 'exit':
+      # self.conn.sendall('Exiti')
       sys.exit(0)
     try:
-      getattr(self, request[0])(*request[1:])
-    except Exception as e:
+      response = getattr(self, request[0])(*request[1:])
+      if not response:
+        response = 'Operation Completed Successfully'
+      self.conn.sendall(response.encode())
+    except:
       print('Invalid function')
       raise TypeError('Crashed')
     finally:
@@ -56,78 +60,102 @@ class Worker(Thread):
 
   def ping(self):
     print('ping called')
-    self.conn.sendall('Runing on {}:{}'.format(self.state.ip, self.state.port).encode())
+    return 'Runing on {}:{}'.format(self.state.ip, self.state.port)
 
   def create_ring(self):
     self.state.successor = self.state.local_address
-    self.state.finger.insert(0, self.state.local_address.__hash__())
+    # self.state.predecessor = self.state.local_address
+    self.state.finger.insert(0, self.state.id)
     self.state.addr_dict[str(self.state.finger[0])] = self.state.local_address
+    return "New ring created"
 
   def join(self, ip, port):
     print('joining {}:{}'.format(ip, port))
     try:
-      s = self.send(Address(ip, port), 'find_successor {} {}'.format(self.state.ip, self.state.port))
+      s = self.send(Address(ip, port), 'find_successor {}'.format(self.state.id))
       data = s.recv(1024).decode('utf-8')
       successor_ip, successor_port = data.split(':')
+      self.state.lock.acquire()
       self.state.successor = Address(successor_ip, successor_port)
-      self.state.finger.insert(0, self.state.successor.__hash__())
+      self.state.finger[0] = self.state.successor.__hash__()
       self.state.addr_dict[str(self.state.finger[0])] = self.state.successor
-      print('successor found to be {}:{}'.format(successor_ip, successor_port))
+      self.state.lock.release()
       s.close()
-      # self.stabilize()
-    except Exception as e:
-      print("Error sending request")
-      raise TypeError('Ha!')
+      self.stabilize()
+      # self.state.predecessor = Address(successor_ip, successor_port)
+      # self.send(self.state.predecessor, 'stabilize').close()
+      return 'successor found to be {}:{}'.format(successor_ip, successor_port)
+    except:
+      raise Exception()
+      return "Error sending request to server"
       # print(e)
 
   def get_successor(self):
-    self.conn.sendall('{}:{}'.format(self.state.successor.ip, self.state.successor.port).encode())
+    return '{}:{}'.format(self.state.successor.ip, self.state.successor.port)
 
   def get_predecessor(self):
-    self.conn.sendall('{}:{}'.format(self.state.predecessor.ip, self.state.predecessor.port).encode())
+    try:
+      return '{}:{}'.format(self.state.predecessor.ip, self.state.predecessor.port)
+    except:
+      return 'None'
 
-  def find_successor(self, ip=None, port=None):
-    id = get_hash('{}:{}'.format(ip, port))
-    if in_range(id, self.state.local_address.__hash__(), self.state.finger[0], end=True):
+  def get_hash(self):
+    return str(self.state.id)
+
+  def find_successor(self, id):
+    id = int(id)
+    # id = get_hash('{}:{}'.format(ip, port))
+    if in_range(id, self.state.id, self.state.finger[0], end=True):
       n_ = self.state.finger[0]
       n_ip = self.state.addr_dict[str(n_)].ip
       n_port = self.state.addr_dict[str(n_)].port
-      self.conn.sendall('{}:{}'.format(n_ip, n_port).encode())
+      return '{}:{}'.format(n_ip, n_port)
     else:
       n_ = self.closest_preceding_node(id)
       n_ip = self.state.addr_dict[str(n_)].ip
       n_port = self.state.addr_dict[str(n_)].port
-      s = self.send(Address(n_ip, n_port), 'find_successor {} {}'.format(ip, port))
-      self.conn.sendall(s.recv(1024))
+      s = self.send(Address(n_ip, n_port), 'find_successor {}'.format(id))
+      return s.recv(1024).decode('utf-8')
 
   def closest_preceding_node(self, id):
-    for i in range(M, 0, -1):
-      if in_range(self.state.finger[i], self.state.local_address.__hash__(), id):
+    for i in range(M-1, 0, -1):
+      if in_range(self.state.finger[i], self.state.id, id):
         return self.state.finger[i]
-    return self.state.local_address.__hash__()
+    return self.state.id
 
   def stabilize(self):
-    s = self.send(self.state.successor, 'get_predecessor')
-    x_ip, x_port = s.recv(1024).decode('utf-8').split(':')
-    s.close()
-    x = Address(x_ip, x_port)
-    if in_range(x.__hash__(), self.state.local_address.__hash__(), self.state.successor.__hash__()):
-      self.state.successor = x
-    self.send(self.state.successor, 'notify {} {}'.format(self.state.ip, self.state.port))
+    try:
+      s = self.send(self.state.successor, 'get_predecessor')
+      x_ip, x_port = s.recv(1024).decode('utf-8').split(':')
+      s.close()
+      x = Address(x_ip, x_port)
+      if in_range(x.__hash__(), self.state.id, self.state.successor.__hash__()):
+        self.state.lock.acquire()
+        self.state.successor = x
+        self.state.lock.release()
+    except: pass
+    self.send(self.state.successor, 'notify {} {}'.format(self.state.ip, self.state.port)).close()
 
   def notify(self, ip, port):
     n_ = Address(ip, port)
     if self.state.predecessor == None or\
-       in_range(n_.__hash__(), self.state.predecessor.__hash__(), self.state.local_address.__hash__()):
-      self.state.predecessor = n_   
+       in_range(n_.__hash__(), self.state.predecessor.__hash__(), self.state.id):
+      self.state.lock.acquire()      
+      self.state.predecessor = n_
+      self.state.lock.release()   
 
   def fix_finger(self):
     self.state.lock.acquire()
     self.state.i = self.state.i + 1
     if self.state.i > M:
       self.state.i = 1
-    # s = self.send(self.state.local_address, 'find_successor {}'.format
-    
+    s = self.send(self.state.local_address, 'find_successor {}'.format((self.state.id + pow(2, self.state.i-1)) % M))
+    n_ip, n_port = s.recv(1024).decode('utf-8').split(':')
+    n = Address(n_ip, n_port)
+    s.close()
+    self.state.addr_dict[str(n.__hash__())] = n
+    self.state.finger[self.state.i] = n.__hash__()
+    self.state.lock.release()
 
   def distribute(self, file):
     pass
