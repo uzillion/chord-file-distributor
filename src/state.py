4631f7697db320b6aaa83740304974fdfacab73c
Uzair Inamdar <uzair_inamdar@hotmail.com>
2019-03-01 16:34:36 -0800

    Add stabilize and notify functions
    Docker changes

--- src/state.py
+++ src/state.py
@@ -2,8 +2,6 @@ import socket
 from address import Address
 from threading import Lock
 
-lock = Lock()
-
 class State:
   def __init__(self, port=8000):
     self.ip = socket.gethostbyname(socket.gethostname())
@@ -13,3 +11,5 @@ class State:
     self.finger = []
     self.addr_dict = {}
     self.local_address = Address(self.ip, self.port)
+    self.i = 1
+    self.lock = Lock()
