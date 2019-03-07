bc87cfe7bdea6eb25db59e3310adb831d7130e05
aaronistheman <aarons.7007@gmail.com>
2019-03-07 15:51:11 -0800

    Maintenance thread seems finished

--- src/maintenance.py
+++ src/maintenance.py
@@ -6,16 +6,11 @@ class Maintenance(Thread):
         super().__init__()
         self.worker = worker
         self.sleep_time = sleep_time  # do maintenance every @sleep_time seconds
-        print("Maintenance.__init()__")
-
-        # for debugging
-        self.num_maintenance_done = 0
 
     def run(self):
         while True:
-            # call stabilize()
-            # call fix_finger()
-            # call check_predecessor()
-            print("I am maintenance thread.", self.num_maintenance_done, flush=True)
-            self.num_maintenance_done += 1
+            # print("I am maintenance thread.", flush=True)
+            self.worker.stabilize()
+            self.worker.fix_finger()
+            self.worker.check_predecessor()
             time.sleep(self.sleep_time)
